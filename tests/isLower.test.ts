835d4200a5c3b205362c30f6b65361fa0eb49af7
Antonio Román <kyradiscord@gmail.com>
2020-07-23 18:17:34 +0200

    test(is-lower): use the right method

--- tests/isLower.test.ts
+++ tests/isLower.test.ts
@@ -1,4 +1,4 @@
-import { fromCode, isLetter, UnicodeCategory } from '../src';
+import { fromCode, isLower, UnicodeCategory } from '../src';
 import { getTestChars, getTestCharsNotInCategory } from './lib/common';
 
 describe('isLower', () => {
@@ -6,13 +6,13 @@ describe('isLower', () => {
 
 	for (const code of getTestChars(...categories)) {
 		test(`GIVEN "${fromCode(code)}" THEN shows true`, () => {
-			expect(isLetter(code)).toBe(true);
+			expect(isLower(code)).toBe(true);
 		});
 	}
 
 	for (const code of getTestCharsNotInCategory(...categories)) {
 		test(`GIVEN "${fromCode(code)}" THEN shows false`, () => {
-			expect(isLetter(code)).toBe(false);
+			expect(isLower(code)).toBe(false);
 		});
 	}
 });
